944c534066db52bdb8951bc0c10c6d8ccd28a3e4
david <pavius.david@gmail.com>
2017-07-25 12:04:04 +0300

    Add request_get_cards method. Rename method with 'request_' prefix

--- Player.py
+++ Player.py
@@ -8,33 +8,49 @@ class Player:
         self.player = player
         self.url = 'http://localhost:1976/'
         self.userID = ''
+        self.cards = list()
 
-    def join(self):
-        data = {"username": self.player}
-        req = urllib2.Request(self.url+"join")
+    def init_request(self, action):
+        req = urllib2.Request(self.url + action)
         req.add_header('Content-Type', 'application/json')
+
+        return req
+
+    def request_join(self):
+        data = {"username": self.player}
+        req = self.init_request("join")
 
         response = urllib2.urlopen(req, json.dumps(data))
 
         dic = json.load(response)
         self.userID = dic["userID"]
 
-    def getstatus(self):
-
-        req = urllib2.Request(self.url+"getStatus")
-        req.add_header('Content-Type', 'application/json')
+    def request_getstatus(self):
         data = {"userID": self.userID}
+        req = self.init_request("getStatus")
 
         response = urllib2.urlopen(req, json.dumps(data))
 
         print response.read()
 
+    def request_get_cards(self):
+        data = {"userID": self.userID}
+        req = self.init_request("getCards")
+
+        response = urllib2.urlopen(req, json.dumps(data))
+
+        dic = json.load(response)
+        self.cards = dic["cards"]
+
+        print self.cards
 
 p = list()
-p.append(Player("pascal"))
-p.append(Player("david"))
+p.append(Player("test4"))
+p.append(Player("test5"))
 
 for i in p:
-    i.join()
-    i.getstatus()
+    i.request_join()
+    i.request_getstatus()
 
+for i in p:
+    i.request_get_cards()
