b889ac8bbc1531cac0ea2f752b5409c0ba2052f3
david <pavius.david@gmail.com>
2017-07-30 15:09:01 +0300

    Replace command attribute by command_handler attribute. Join game in constructor

--- strategies/StrategyD0.py
+++ strategies/StrategyD0.py
@@ -1,10 +1,11 @@
-from request import Command
+from request import CommandHandler
 
 
 class StrategyD0:
 
     def __init__(self, bot=None):
-        self.command = Command.Command()
+        self.command_handler = CommandHandler.RequesterHandler()
+        bot.name = self.command_handler.join(bot.name)
 
     def play(self):
         return False
