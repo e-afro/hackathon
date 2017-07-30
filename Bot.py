551a30f8af80a007efc788a08ebcfee98fbbaaf7
david <pavius.david@gmail.com>
2017-07-30 23:35:04 +0300

    Move commandHandler from StrategyD0 to Bot

--- Bot.py
+++ Bot.py
@@ -1,13 +1,15 @@
 from strategies import StrategyD0
 from game import State
+from request import CommandHandler
 
 
 class Bot:
 
     DEFAULT_STRATEGY = StrategyD0.StrategyD0
 
-    def __init__(self, strategy=None):
-        self.name = ''
+    def __init__(self, name='bot', strategy=None):
+        self.command_handler = CommandHandler.CommandHandler()
+        self.name = self.command_handler.join(name)
         self.state = State.State()
         self.strategy = strategy(self) if strategy else self.build_strategy()
 
--- strategies/StrategyD0.py
+++ strategies/StrategyD0.py
@@ -1,16 +1,13 @@
-from request import CommandHandler
 from game import Status
 
 
 class StrategyD0:
 
     def __init__(self, bot):
-        self.command_handler = CommandHandler.CommandHandler()
-        bot.name = self.command_handler.join(bot.name)
         self.status = Status.Status()
 
     def play_card(self):
-        None
+        return None
 
     def select_row(self):
         None
