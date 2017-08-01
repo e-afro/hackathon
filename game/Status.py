3a3a31d79bf7b7160f3af08ed26a6da346e48209
david <pavius.david@gmail.com>
2017-08-01 18:46:19 +0200

    Add attr hand in Status. load build cards too

--- game/Status.py
+++ game/Status.py
@@ -1,5 +1,6 @@
 import Board
 import Deck
+import Hand
 
 
 class Status:
@@ -7,12 +8,16 @@ class Status:
     def __init__(self):
         self.deck = Deck.Deck()
         self.board = Board.Board()
+        self.hand = Hand.Hand()
 
     def load(self, response):
         card_values = response["cardvalues"]
         rows = response["rows"]
+        cards = response["cards"]
 
         if card_values is not None:
             self.deck.build_all_cards(card_values)
-        if rows is not None or not None in rows:
+        elif rows is not None or None not in rows:
             self.board.build_rows(self.deck, rows)
+        elif cards is not None:
+            self.hand.build_hand(cards)
